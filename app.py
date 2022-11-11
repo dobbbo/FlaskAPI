e7a240d0aef6f308f26bc20e298d394e23bc0173
dobbbo <dobson.adam21@gmail.com>
2022-11-12 00:01:00 +1100

    Added initial endpoints.

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
 import psycopg2
+from datetime import datetime, timezone
 from dotenv import load_dotenv
 from flask import Flask, request
 
@@ -7,7 +8,7 @@ CREATE_STATIONS_TABLE = (
     "CREATE TABLE IF NOT EXISTS stations (id SERIAL PRIMARY KEY, name TEXT);"
 )
 
-CREATE_PRICES_TABLE = """CREATE TABLE IF NOT EXISTS prices (station_id TEXT, price REAL, date TIMESTAMP, FOREIGN KEY (station_id) REFERENCES stations(id) ON DELETE CASCADE);"""
+CREATE_PRICES_TABLE = """CREATE TABLE IF NOT EXISTS prices (station_id INTEGER, price REAL, date TIMESTAMP, FOREIGN KEY (station_id) REFERENCES stations(id) ON DELETE CASCADE);"""
 
 INSERT_STATION_RETURN_ID = "INSERT INTO stations (name) VALUES (%s) RETURNING id;"
 
@@ -30,3 +31,37 @@ connection = psycopg2.connect(url)
 @app.post("/api/station")
 def create_station():
     data = request.get_json()
+    name = data["name"]
+    with connection:
+        with connection.cursor() as cursor:
+            cursor.execute(CREATE_STATIONS_TABLE)
+            cursor.execute(INSERT_STATION_RETURN_ID, (name,))
+            station_id = cursor.fetchone()[0]
+    return {"id": station_id, "message": f"{name} petrol station created."}, 201
+
+@app.post("/api/price")
+def add_price():
+    data = request.get_json()
+    price = data["price"]
+    station_id = data["station"]
+    try:
+        date = datetime.strptime(data["date"], "%d-%m-%Y %H:%M:%S")
+    except KeyError:
+        date = datetime.now(timezone.utc)
+
+    with connection:
+        with connection.cursor() as cursor:
+            cursor.execute(CREATE_PRICES_TABLE)
+            cursor.execute(INSERT_PRICE, (station_id, price, date))
+    
+    return {"message": "Petrol price added."}, 201
+
+@app.get("/api/average")
+def get_average_price():
+    with connection:
+        with connection.cursor() as cursor:
+            cursor.execute(AVERAGE_PRICE)
+            average = cursor.fetchone()[0]
+            cursor.execute(NUMBER_OF_DAYS)
+            days = cursor.fetchone()[0]
+    return {"average": round(average, 2), "days": days}
